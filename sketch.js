655e66e145d0c32bba7a2ab71bd14c438590856c
yurism <mascarenhasyuri@hotmail.com>
2024-04-18 09:17:55 -0300

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+const
+
+function preload() {
+    // Code in this function runs before setup().
+}
+function setup() {
+    // Code in this function runs once before draw().
+    createCanvas(500, 500);
+}
+function draw() {
+    // Code in this function runs every frame.
+    background(200);
+}
