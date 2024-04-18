07dfd672cfb06ddb9495ac577d74894bd599014e
yurism <mascarenhasyuri@hotmail.com>
2024-04-18 19:50:57 -0300

    [Add] New class to sketch

--- sketch.js
+++ sketch.js
@@ -1,13 +1,23 @@
-const
+let str1;
+let str2;
 
-function preload() {
-    // Code in this function runs before setup().
-}
 function setup() {
-    // Code in this function runs once before draw().
-    createCanvas(500, 500);
+  createCanvas(500, 500);
+  str1 = new TextObj("fiz essa para os meus ami__s", 0, 0, 20, 255, LEFT, TOP);
+  str2 = new TextObj(
+    "hoje estão todos m____",
+    width,
+    height,
+    20,
+    255,
+    RIGHT,
+    BOTTOM
+  );
 }
 function draw() {
-    // Code in this function runs every frame.
-    background(200);
+  background(0);
+
+  // Texts
+  str1.display();
+  str2.display();
 }
